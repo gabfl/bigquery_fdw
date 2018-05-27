677e1fca37f777696749f30b19a3cb76d99d5906
gab <gabrielbordeaux@gmail.com>
2018-05-27 14:31:56 -0400

    Add SQL file for travis and docker

---
+++ src/unittest/utils/sql/setup.sql
@@ -0,0 +1,22 @@
+-- Create extension
+CREATE EXTENSION multicorn;
+
+-- Create server
+CREATE SERVER bigquery_srv FOREIGN DATA WRAPPER multicorn
+OPTIONS (
+    wrapper 'bigquery_fdw.fdw.ConstantForeignDataWrapper'
+);
+
+-- Create sample table
+CREATE FOREIGN TABLE usa_names (
+    state text,
+    gender text,
+    year bigint,
+    name text,
+    number bigint
+) SERVER bigquery_srv
+OPTIONS (
+    fdw_dataset  'bigquery-public-data.usa_names',
+    fdw_table 'usa_1910_current',
+    fdw_key '/opt/key/key.json'
+);
