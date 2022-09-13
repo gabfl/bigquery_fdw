1dbcfe149f4cb6725de4c151921667a70163d4de
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2022-09-13 14:12:41 -0400

    Update google-auth to v. 2.9.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # external dependencies
     install_requires=[
         'google-cloud-bigquery==3.3.2',
-        'google-auth==2.9.0',
+        'google-auth==2.9.1',
         # 'google-auth==2.11.0',
         'google-auth-oauthlib==0.5.2',
         'protobuf==3.20.1',  # Forcing version for google-cloud-bigquery
