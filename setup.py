8400761f6c5c4ca7a84bc6c76715ac16f0972ca1
Thomas Johnson <thomascjohnson@gmail.com>
2023-06-21 15:05:27 +0200

    Upgrades to use BigQuery Storage client

--- setup.py
+++ setup.py
@@ -2,47 +2,46 @@ from setuptools import setup
 
 try:
     import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
+
+    long_description = pypandoc.convert_file("README.md", "rst")
 except (IOError, ImportError):
-    long_description = open('README.md').read()
+    long_description = open("README.md").read()
 
 setup(
-    name='bigquery-fdw',
-    version='2.1.1',
-    description='BigQuery Foreign Data Wrapper for PostgreSQL',
+    name="bigquery-fdw",
+    version="2.2.1",
+    description="BigQuery Foreign Data Wrapper for PostgreSQL",
     long_description=long_description,
-    author='Gabriel Bordeaux',
-    author_email='pypi@gab.lc',
-    url='https://github.com/gabfl/bigquery_fdw',
-    license='MIT',
-    packages=['bigquery_fdw'],
-    package_dir={'bigquery_fdw': 'src'},
+    author="Gabriel Bordeaux",
+    author_email="pypi@gab.lc",
+    url="https://github.com/gabfl/bigquery_fdw",
+    license="MIT",
+    packages=["bigquery_fdw"],
+    package_dir={"bigquery_fdw": "src"},
     # external dependencies
     install_requires=[
-        'google-cloud-bigquery==3.10.0',
-        'google-auth==2.17.3',
-        'google-auth-oauthlib==1.0.0',
-        'protobuf==4.21.8',  # Forcing version for google-cloud-bigquery
-        'grpcio==1.50.0',  # Forcing version for google-cloud-bigquery
-        'grpcio-status==1.50.0',  # Forcing version for google-cloud-bigquery
+        "google-cloud-bigquery==3.11.1",
+        "google-cloud-bigquery-storage==2.20.0",
+        "db-dtypes==1.1.1",
+        "pandas==2.0.2",
     ],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             # 'bigquery_fdw = bigquery_fdw.fdw:main',
-            'bq_client_test = bigquery_fdw.bqclient_test:main',
+            "bq_client_test = bigquery_fdw.bqclient_test:main",
         ],
     },
     classifiers=[  # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Topic :: Database',
-        'Topic :: Database :: Database Engines/Servers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS',
-        'Operating System :: POSIX :: Linux',
-        'Natural Language :: English',
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Natural Language :: English",
         #  'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
         #  'Development Status :: 5 - Production/Stable',
     ],
 )
--- src/bqclient.py
+++ src/bqclient.py
@@ -7,7 +7,7 @@
 
 import datetime
 
-from google.cloud import bigquery
+from google.cloud import bigquery, bigquery_storage
 from google import auth
 
 
@@ -20,25 +20,26 @@ class BqClient:
 
     def setClient(self):
         """
-            Set BigQuery client with a Json key
+        Set BigQuery client with a Json key
         """
 
         try:
             self.client = bigquery.Client()
         except auth.exceptions.DefaultCredentialsError as e:
             raise RuntimeError(
-                'BigQuery client is not instantiated properly: ' + str(e))
+                "BigQuery client is not instantiated properly: " + str(e)
+            )
 
     def getClient(self):
         """
-            Returns `client`
+        Returns `client`
         """
 
         return self.client
 
-    def runQuery(self, query, parameters=[], sqlDialect='standard'):
+    def runQuery(self, query, parameters=[], sqlDialect="standard"):
         """
-            Run BigQuery query
+        Run BigQuery query
         """
 
         if self.client:
@@ -48,7 +49,8 @@ class BqClient:
                 job_config.query_parameters = parameters
 
                 self.queryJob = self.client.query(
-                    query, job_config=job_config, location=None)
+                    query, job_config=job_config, location=None
+                )
             else:  # Non parameterized query
                 self.queryJob = self.client.query(query, location=None)
 
@@ -61,54 +63,60 @@ class BqClient:
 
         else:
             raise RuntimeError(
-                'BigQuery client is not instantiated properly (from `runQuery`).')
+                "BigQuery client is not instantiated properly (from `runQuery`)."
+            )
 
     def getQueryJob(self):
         """
-            Returns `queryJob`
+        Returns `queryJob`
         """
 
         return self.queryJob
 
     def readResult(self):
         """
-            Returns query result
+        Returns query result
         """
 
         if self.queryJob:
             result = self.queryJob.result()
-            return list(result)
+            return result.to_dataframe(create_bqstorage_client=True).to_dict(
+                orient="records"
+            )
         else:
-            raise RuntimeError('No query is pending a result.')
+            raise RuntimeError("No query is pending a result.")
 
     def setParameter(self, type_, value):
         """
-            Prepare a parameter for a parameterized query
-            As documented by Google, only standard SQL syntax supports parameters in queries
+        Prepare a parameter for a parameterized query
+        As documented by Google, only standard SQL syntax supports parameters in queries
 
-            See https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/bigquery/cloud-client/query_params.py
+        See https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/bigquery/cloud-client/query_params.py
         """
 
         return bigquery.ScalarQueryParameter(
             # Set the name to None to use positional parameters (? symbol
             # in the query).  Note that you cannot mix named and positional
             # parameters.
-            None, type_, self.varToString(value))
+            None,
+            type_,
+            self.varToString(value),
+        )
 
     def varToString(self, var):
         """
-            Format a variable to a string
+        Format a variable to a string
 
-            Examples:
-            `varToString(1234)` -> '1234'
-            `varToString('1234')` -> '1234'
-            `varToString(datetime.datetime.now())` -> '2017-10-03 11:47:54'
-            `varToString(datetime.datetime.now().date())` -> '2017-10-03'
+        Examples:
+        `varToString(1234)` -> '1234'
+        `varToString('1234')` -> '1234'
+        `varToString(datetime.datetime.now())` -> '2017-10-03 11:47:54'
+        `varToString(datetime.datetime.now().date())` -> '2017-10-03'
         """
 
         if type(var) is datetime.date:
-            return var.strftime('%Y-%m-%d')
+            return var.strftime("%Y-%m-%d")
         elif type(var) is datetime.datetime:
-            return var.strftime('%Y-%m-%d %H:%M:%S')
+            return var.strftime("%Y-%m-%d %H:%M:%S")
 
         return str(var)
