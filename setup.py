f69317fefdc8b4a3f8525d6a05fdd5f37375cc5f
Thomas Johnson <thomascjohnson@gmail.com>
2023-06-22 12:37:42 +0200

    Fixes version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, ImportError):
 
 setup(
     name="bigquery-fdw",
-    version="2.2.1",
+    version="2.2.0",
     description="BigQuery Foreign Data Wrapper for PostgreSQL",
     long_description=long_description,
     author="Gabriel Bordeaux",
