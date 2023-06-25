2cd058a21b33dffffc87cfe4a3cd3093eefb4d22
Thomas Johnson <thomascjohnson@gmail.com>
2023-06-25 23:49:31 +0200

    == not =

--- src/bqclient.py
+++ src/bqclient.py
@@ -119,7 +119,7 @@ class BqClient:
             param_value = var.strftime("%Y-%m-%d %H:%M:%S")
         elif type(var) in (int, float):
             param_value = var
-        elif var.lower() = 'null':
+        elif var.lower() == "null":
             param_value = None
         else:
             param_value = str(var)
