543e00c902cdbdd6b2ca39b4b2080ff236a4e0d6
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2023-06-26 21:34:13 -0400

    Upgrade Google dependencies (#36)
    * Upgrade Google dependencies
    
    * Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except (IOError, ImportError):
 
 setup(
     name='bigquery-fdw',
-    version='2.1.1',
+    version='2.1.2',
     description='BigQuery Foreign Data Wrapper for PostgreSQL',
     long_description=long_description,
     author='Gabriel Bordeaux',
@@ -19,8 +19,8 @@ setup(
     package_dir={'bigquery_fdw': 'src'},
     # external dependencies
     install_requires=[
-        'google-cloud-bigquery==3.10.0',
-        'google-auth==2.17.3',
+        'google-cloud-bigquery==3.11.2',
+        'google-auth==2.21.0',
         'google-auth-oauthlib==1.0.0',
         'protobuf==4.21.8',  # Forcing version for google-cloud-bigquery
         'grpcio==1.50.0',  # Forcing version for google-cloud-bigquery
